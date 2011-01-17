db2d53bdfd1aa093942d040cf38e690fbdbdade7
Mike Heffner <mikeh@fesnel.com>
2011-01-17 13:59:33 -0500

    Bootstrap openvpn on ubuntu 8.04.

--- lib/openvpn_config/bootstrap.rb
+++ lib/openvpn_config/bootstrap.rb
@@ -27,6 +27,21 @@ module Bootstrap
 			EOF_CAT
 			#{epel_base_url_add_command}
 			yum install -y openvpn
+		elif [ -f /etc/debian_version -a `lsb_release -cs` == "hardy" ]; then
+			# Hardy image needs an update
+			DEBIAN_FRONTEND=noninteractive apt-get update
+
+			DEBIAN_FRONTEND=noninteractive apt-get install -y openvpn &> /dev/null || { echo "Failed to install OpenVPN via apt-get on $HOSTNAM
+			# No chkconfig on Ubuntu 8.04
+			cat > /sbin/chkconfig <<-"EOF_CAT"
+			#!/bin/bash
+			if [ \$2 == "on" ]; then
+				update-rc.d \$1 defaults
+			else
+				update-rc.d -f \$1 remove
+			fi
+			EOF_CAT
+			chmod +x /sbin/chkconfig
 		elif [ -f /etc/debian_version ]; then
 			DEBIAN_FRONTEND=noninteractive apt-get install -y openvpn &> /dev/null || { echo "Failed to install OpenVPN via apt-get on $HOSTNAME."; exit 1; }
 			DEBIAN_FRONTEND=noninteractive apt-get install -y chkconfig &> /dev/null || { echo "Failed to install chkconfig via apt-get on $HOSTNAME."; exit 1; }
@@ -35,6 +50,9 @@ module Bootstrap
 			exit 1
 		fi
 		SCRIPT_EOF
+          File.open("/tmp/openvpn_script_#{rand 999}", "w") do |f|
+            f << script
+          end
 return Util::Ssh.run_cmd(@external_ip_addr, script, @ssh_as_user, @ssh_identity_file, @logger)
 
 	end
