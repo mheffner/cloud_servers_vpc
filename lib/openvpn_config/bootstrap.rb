d6c99f12d90e39c44eff7f17f316205df4ea5c26
QA User <qa@librato.com>
2011-02-09 12:28:38 -0800

    Move bootstrap script to a separate ERB shell-script template.

--- lib/openvpn_config/bootstrap.rb
+++ lib/openvpn_config/bootstrap.rb
@@ -1,5 +1,6 @@
 module OpenvpnConfig
 
+require 'erb'
 require 'util/ssh'
 
 module Bootstrap
@@ -14,31 +15,10 @@ module Bootstrap
 			epel_base_url_add_command="sed -e \"s|#baseurl=EPEL_BASE_URL|baseurl=#{ENV['EPEL_BASE_URL']}|g\" -i /etc/yum.repos.d/epel.repo"
         end
 
-        script=<<-SCRIPT_EOF
-		if [ -f /etc/fedora-release ]; then
-			yum install -y openvpn
-		elif [ -f /etc/redhat-release ]; then
-			if [ -n "#{ENV['EPEL_BASE_URL']}" ]; then
-				cat > /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL <<-"EOF_CAT"
-				#{IO.read(File.join(File.dirname(__FILE__), "RPM-GPG-KEY-EPEL"))}
-				EOF_CAT
-				cat > /etc/yum.repos.d/epel.repo <<-"EOF_CAT"
-				#{IO.read(File.join(File.dirname(__FILE__), "epel.repo"))}
-				EOF_CAT
-				#{epel_base_url_add_command}
-			else
-				rpm -Uvh http://download.fedora.redhat.com/pub/epel/5/x86_64/epel-release-5-4.noarch.rpm
-			fi
-			yum install -y openvpn
-		elif [ -f /etc/debian_version ]; then
-			DEBIAN_FRONTEND=noninteractive apt-get install -y openvpn &> /dev/null || { echo "Failed to install OpenVPN via apt-get on $HOSTNAME."; exit 1; }
-			DEBIAN_FRONTEND=noninteractive apt-get install -y chkconfig &> /dev/null || { echo "Failed to install chkconfig via apt-get on $HOSTNAME."; exit 1; }
-			sed -e "s|.*HashKnownHosts.*|    HashKnownHosts no|g" -i /etc/ssh/ssh_config
-		else
-			echo "Unable to install openvpn package."
-			exit 1
-		fi
-		SCRIPT_EOF
+          bootf = File.read(File.join(File.dirname(__FILE__), "bootstrap.sh.erb"))
+          script = ""
+          b = binding
+          ERB.new(bootf, 0, "", "script").result(b)
 return Util::Ssh.run_cmd(@external_ip_addr, script, @ssh_as_user, @ssh_identity_file, @logger)
 
 	end
